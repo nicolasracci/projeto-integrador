8a123f54b8a2625acb4f81c44328a4a08f69eef4
Nícolas <115915141+nicolasracci@users.noreply.github.com>
2022-10-16 03:22:19 -0300

    iniciando projeto

---
+++ GestaoServicos/CompraServico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ProjetoIntegradorSenac.GestaoServicos
+{
+    class CompraServico
+    {
+        private static void Servicos()
+        {
+            // criar uma lista com os serviços da empresa
+        }
+    }
+}
